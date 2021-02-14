e7b1fa2bc1ad96635e61e149e2edd1e360a13c38
Linkerin <box4dev@mail.ru>
2021-02-14 17:13:14 +0300

    JS for drag&drop behaviour was almost completed

--- static/main.js
+++ static/main.js
@@ -1,13 +1,35 @@
 let button = document.querySelector('.btn');
-let upload_area = document.querySelector('.drop-area');
+let uploadArea = document.querySelector('.drop-area');
+let filesArea = document.querySelector('.uploaded-files');
+let errorMsg = document.querySelector('.error-msg');
+let firstFile = document.querySelector('#first-file-name');
+let secondFile = document.querySelector('#second-file-name');
 
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    upload_area.addEventListener(eventName, preventDefaults, false)
-  })
-  function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
+uploadArea.addEventListener('click', e => document.querySelector('#upload').click());
+
+uploadArea.addEventListener('dragover', e => {
+  e.preventDefault();
+  uploadArea.classList.add('drop-area__over')
+});
+
+['dragleave', 'dragend'].forEach(type =>{
+  uploadArea.addEventListener(type, e => uploadArea.classList.remove('drop-area__over'));
+});
+
+uploadArea.addEventListener('drop', e => {
+  e.preventDefault();
+  if (e.dataTransfer.files.length == 2) {
+    // inputElement.files = e.dataTransfer.files;
+    console.log(e.dataTransfer.files);
+    firstFile.textContent = e.dataTransfer.files[0].name;
+    secondFile.textContent = e.dataTransfer.files[1].name;
+    uploadArea.style.display = 'none';
+    filesArea.style.display = 'flex';
+  } else {
+    uploadArea.classList.remove('drop-area__over');
+    errorMsg.style.display = 'block';
+    setTimeout(() => errorMsg.style.display = 'none', 3000);
   }
+});
 
-upload_area.addEventListener('drop', () =>  console.log('Hello'))
 button.addEventListener('click', () => alert('Hello!'));
