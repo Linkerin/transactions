d1c7430251b33c9c2de5ba0020a80ab47f1f6350
Linkerin <box4dev@mail.ru>
2021-02-18 00:55:04 +0300

    Analysis itself added. Needs some fixes

--- transapp.py
+++ transapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify
-from werkzeug.utils import secure_filename
-import json
+from analysis import Transactions
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +15,21 @@ def upload():
     if request.method == 'POST':
         for _, value in request.files.items():
             value.save(f'./temp/{value.filename}')
+
+    files = os.listdir('./temp')
+    if len(files) == 2:
+        transactions = Transactions()
+        df = transactions.upload(f'./temp/{files[0]}', f'./temp/{files[1]}')
+
+        if type(df) != str:
+            result = transactions.full_analysis(df)
+            result.to_excel('./temp/result.xlsx')
+        else:
+            return jsonify(status=df)
+
+    for item in files:
+        os.remove(f'./temp/{item}')
+
     return jsonify(status='OK')
 
 
