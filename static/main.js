51e86fdc00d15699ca9ef38c9fc7f9776d23bf65
Linkerin <box4dev@mail.ru>
2021-02-26 00:43:18 +0300

    Interim logic of analysis process

--- static/main.js
+++ static/main.js
@@ -6,6 +6,7 @@ let filesAreaClick = document.querySelector('#upload')
 let errorMsg = document.querySelector('.error-msg');
 let firstFile = document.querySelector('#first-file-name');
 let secondFile = document.querySelector('#second-file-name');
+let filesToAnalyse = {filenames: []};
 
 async function fileUpload(e) {
   let files;
@@ -27,17 +28,21 @@ async function fileUpload(e) {
       const response = await fetch('/upload', {
         method: 'POST',
         body: formData
-      });
-      if (response) {
-        let msg = await response.json();
-        console.log(msg);
-        console.log('Upload completed');
-      }
+      }).then(res => res.json());
 
       firstFile.textContent = files[0].name;
       secondFile.textContent = files[1].name;
       uploadArea.style.display = 'none';
       filesArea.style.display = 'flex';
+      
+      if (response.status === 'Upload completed') {
+        console.log(response)
+        filesToAnalyse.filenames = response.filenames;
+        // const pandas = await fetch('/pandas_upload', {
+        //   method: 'POST',
+        //   body: JSON.stringify({filenames: response.filenames})
+        // }).then(res => res.json()).then(res => console.log(res));
+      }
 
     } else {
       uploadArea.classList.remove('drop-area__over');
@@ -63,7 +68,41 @@ uploadArea.addEventListener('drop', fileUpload)
 uploadArea.addEventListener('click', e => filesAreaClick.click());
 filesAreaClick.addEventListener('change', fileUpload)
 
-button.addEventListener('click', () => alert('Hello!'));
+// async function pandasUpload(e) {
+//   e.preventDefault;
+//   if (filesToAnalyse.filenames.length) {
+//     try {
+//       const pandas = await fetch('/pandas_upload', {
+//         method: 'POST',
+//         body: JSON.stringify(filesToAnalyse)
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   } else {
+//     errorMsg.textContent = 'Файлы не были загружены на сервер'
+//     errorMsg.style.display = 'block';
+//     setTimeout(() => errorMsg.style.display = 'none', 3000);
+//   }
+// }
+// button.addEventListener('click', pandasUpload);
+button.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (filesToAnalyse.filenames.length) {
+    try {
+      const pandas = await fetch('/pandas_upload', {
+        method: 'POST',
+        body: JSON.stringify(filesToAnalyse)
+      }).then(res => console.log(res));
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    errorMsg.textContent = 'Файлы не были загружены на сервер'
+    errorMsg.style.display = 'block';
+    setTimeout(() => errorMsg.style.display = 'none', 2000);
+  }
+});
 
 // Information sidebar
 let info = document.querySelector('.info-icon')
--- transapp.py
+++ transapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, send_from_directory, jsonify
 from analysis import Transactions
+import json
 import os
 
 app = Flask(__name__)
@@ -10,7 +11,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/upload", methods = ['POST', 'GET'])
+@app.route("/upload", methods = ['POST'])
 def upload():
     if request.method == 'POST':
         filenames = []
@@ -20,17 +21,29 @@ def upload():
 
     return jsonify(status='Upload completed', filenames=filenames)
 
+@app.route("/pandas_upload", methods= ['GET', 'POST'])
+def pandas_upload():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+
+        valid_types = ['xls', 'xlsx']
+        for item in data['filenames']:
+            if item.split('.')[-1] not in valid_types:
+                return jsonify(status='Pandas: invalid file type')
+
+        transactions = Transactions()
+        df = transactions.upload(f"./temp/{data['filenames'][0]}", f"./temp/{data['filenames'][1]}")
+        if type(df) != str:
+            result = transactions.full_analysis(df)
+            result.to_excel('./temp/result.xlsx')
+        else:
+            return jsonify(status=df)
+    print('Done')
+    return send_from_directory('./temp', filename='result.xlsx', as_attachment=True,
+                     cache_timeout=0,
+                     mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
     # files = os.listdir('./temp')
-    # if len(files) == 2:
-    #     transactions = Transactions()
-    #     df = transactions.upload(f'./temp/{files[0]}', f'./temp/{files[1]}')
-
-    #     if type(df) != str:
-    #         result = transactions.full_analysis(df)
-    #         result.to_excel('./temp/result.xlsx')
-    #     else:
-    #         return jsonify(status=df)
-
     # for item in files:
     #     os.remove(f'./temp/{item}')
 
